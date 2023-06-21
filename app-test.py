47a8542c6b0ae048348ebb516f7cde45ad996c88
JoeyRagheb <101679327+JoeyRagheb@users.noreply.github.com>
2023-06-21 19:50:35 -0400

    r

--- app-test.py
+++ app-test.py
@@ -60,7 +60,7 @@ def run():
             st.info(r)
 
             second = result[1].numpy()
-            # second = np.moveaxis(second, -1, 0)
+            second = second.reshape(240,240,32)
             s = second.shape
             st.info(s)
             st.image(second)
