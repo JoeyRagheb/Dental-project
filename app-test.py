ecde3ba70cb36cbf549d1f792231e9c22c18353c
JoeyRagheb <101679327+JoeyRagheb@users.noreply.github.com>
2023-06-21 18:48:31 -0400

    t

--- app-test.py
+++ app-test.py
@@ -14,112 +14,47 @@ import torchvision
 import tensorflow
 
 
-# from keras.models import load_model
-# import requests
-# from bs4 import BeautifulSoup
-
-# vegetables = ['Beetroot', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Corn', 'Cucumber', 'Eggplant', 'Ginger',
-#               'Lettuce', 'Onion', 'Peas', 'Potato', 'Raddish', 'Soy Beans', 'Spinach', 'Sweetcorn', 'Sweetpotato',
-#               'Tomato', 'Turnip']
-
-
-# def fetch_calories(prediction):
-#     try:
-#         url = 'https://www.google.com/search?&q=calories in ' + prediction
-#         req = requests.get(url).text
-#         scrap = BeautifulSoup(req, 'html.parser')
-#         calories = scrap.find("div", class_="BNeawe iBp4i AP7Wnd").text
-#         return calories
-#     except Exception as e:
-#         st.error("Can't able to fetch the Calories")
-#         print(e)
 
 
 def processed_img(img_path):
-    # img = load_img(img_path)
     img = load_img(img_path, target_size=(960, 960, 3))
     img = img_to_array(img)
-
-    st.info('***HERE***')
-    st.info(type(img))
-    f = img.shape
-    st.info(f)
-
 
     tensor_image = torch.from_numpy(img)
     tensor_image = tensor_image.permute(2, 0, 1)
     tensor_image = tensor_image.unsqueeze(0)
 
-    st.info('***HERE2***')
-    st.info(type(tensor_image))
-    f = tensor_image.shape
-    st.info(f)
-
-    # img = img_to_array(img)
-    # img = np.moveaxis(img, -1, 0)
-    # img = np.expand_dims(img, [0])
-    # img = img / 255
-    # img = np.expand_dims(img, [0])
 
     img = tensor_image
-    
-
-
-    # import torch
 
     torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
     #Model
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='Best/best.pt', force_reload=True)
-    # model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/yolov5s.pt', force_reload=True)
-    # model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/best.pt', force_reload=True)
-    # model = torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload=True)
 
-    # #Images
-    # img = DataLoader()
+
+    #Images
+    img = DataLoader()
 
     #Inference
     results = model(img)
 
-    # #Results
-    # results.print() # or .show(), .save(), .crop(), .pandas(), etc.
-    # results.xywh[0]
-
-
-
-    # res ="carrot"
-    # # res should be a string
-    # print(res)
-    # return res.capitalize()
     return results
 
 def run():
     st.title("Maha Cavity Finder")
     img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
     if img_file is not None:
-        # st.info('**TESTING00**')
-        # st.info(type(img_file))
         img = Image.open(img_file).resize((250, 250))
-        # st.info(type(img))
         st.image(img, use_column_width=False)
-        # save_image_path = './' + img_file.name
         save_image_path = img_file.name
-        # st.info(type(save_image_path))
         with open(save_image_path, "wb") as f:
             f.write(img_file.getbuffer())
 
         # if st.button("Predict"):
         if img_file is not None:
             result = processed_img(save_image_path)
-            # print(result)
-            # if result in vegetables:
-            #     st.info('**Category : Vegetables**')
-            # else:
-            #     st.info('**Category : Fruit**')
-            # st.success("**Predicted : " + result + '**')
-            # # cal = fetch_calories(result)
-            # # if cal:
-            # #     st.warning('**' + cal + '(100 grams)**')
-            
+            st.image(result)
+
             st.info('**TESTING**')
             st.info(type(result))
             # st.info(result)
@@ -127,105 +62,6 @@ def run():
             st.info(len(result[0]))
             st.info(result[-1][-1][-1][-1][-1])
 
-            result = torch.tensor(result)
-            st.info('**TESTING11**')
-            st.info(type(result))
-            f = result.shape
-            st.info(f)
-            st.image(result)
             
-            
-
-
-
-
-
-
-
-
-
-
-
-            import utils
-            display = utils.notebook_init()
-            var = display.Image(result)
-            
-            # from IPython.display import Image
-            st.image(tensorflow.make_ndarray(tensorflow.make_tensor_proto(result)))
-
-            
-            
-            
-            var = transforms.ToPILImage()(var)
-
-            grid = torchvision.utils.make_grid(var)
-
-            st.image(grid)
-            
-            
-            
-
-
-
-
-            result = reduce(lambda x,y: T.cat((x,y)), result[:-1])
-            st.info(type(result))
-            f = result.shape
-            st.info(f)
-
-
-
-
-
-
-            result = np.squeeze(result)
-            st.info(type(result))
-            # st.info(result)
-            f = result.shape
-            st.info(f)
-
-
-            st.info('**TESTING2**')
-            # Torch
-            result = result[0]
-            f = result.shape
-            st.info(f)
-            st.info(type(result))
-            result = torch.Tensor(result)
-            st.info(type(result))
-
-            
-            st.info('**HERE4**')
-            image = image.permute((2, 0, 1))  # convert to (C, H, W) format
-            image = image.unsqueeze(0)  # add fake batch dimension
-            resize = Resize((960, 960))
-            new_image = resize(image)
-            st.info(type(result))
-            f = result.shape
-            st.info(f)
-
-
-
-            #numpy
-            result = result.numpy()
-            f = result.shape
-            st.info(f)
-            st.info(type(result))
-            st.image(result)
-
-
-            # result = np.array(result, dtype=np.float32)
-            # st.info(type(result))
-            # st.info(result)
-
-            # result = torch.Tensor(result)
-            # st.info(type(result))
-            # result = result.permute(1, 2, 0).numpy()
-            # st.info(result)
-            # st.image(result)
-            # # result = result.numpy()
-            # # result = np.squeeze(result)
-            # st.image(result)
-
 
 run()
