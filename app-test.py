3478d2a80c59a83a042ec68dc8bfca9cf31bc389
JoeyRagheb <101679327+JoeyRagheb@users.noreply.github.com>
2023-06-21 14:29:42 -0400

    t

--- app-test.py
+++ app-test.py
@@ -115,7 +115,7 @@ def run():
             st.info(len(result))
             st.info(len(result[0]))
 
-            result = torch.cat([x.float() for x in result], dim=1).half()
+            result = torch.cat([result[0], result[1]], dim=2)
             st.info(type(result))
             f = result.shape
             st.info(f)
