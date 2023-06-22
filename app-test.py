b9af07ae37a487726827233328446890191db547
JoeyRagheb <101679327+JoeyRagheb@users.noreply.github.com>
2023-06-22 17:36:06 -0400

    test

--- app-test.py
+++ app-test.py
@@ -13,27 +13,34 @@ from torchvision import transforms
 import torchvision
 import tensorflow
 import cv2
+import os
+from ultralytics import YOLO
 
 
 
 def processed_img(img_path):
     img = load_img(img_path, target_size=(960, 960, 3))
 
-    img = img_to_array(img)
-    tensor_image = torch.from_numpy(img)
-    tensor_image = tensor_image.permute(2, 0, 1)
-    tensor_image = tensor_image.unsqueeze(0)
-    img = tensor_image
-    st.info("***THISSS***")
-    st.info(type(img))
-    f = img.shape
-    st.info(f)
+
+    os.system('yolo task=segment mode=predict model=weights/best.pt conf=0.25 source=test-image/ save=true')
+
+    for image_path in glob.glob(f'{HOME}/runs/segment/predict/*.jpg')[:3]:
+      st.image(Image(filename=image_path, height=600))
+    # img = img_to_array(img)
+    # tensor_image = torch.from_numpy(img)
+    # tensor_image = tensor_image.permute(2, 0, 1)
+    # tensor_image = tensor_image.unsqueeze(0)
+    # img = tensor_image
+    # st.info("***THISSS***")
+    # st.info(type(img))
+    # f = img.shape
+    # st.info(f)
 
 
 
-    torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
-    #Model
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='Best/best.pt', force_reload=True)
+    # torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
+    # #Model
+    # model = torch.hub.load('ultralytics/yolov5', 'custom', path='Best/best.pt', force_reload=True)
 
     #Inference
     # results = model(img)
@@ -55,43 +62,7 @@ def run():
         if img_file is not None:
             result = processed_img(save_image_path)
 
-            # Convert tensors in result[0] to NumPy arrays
-            result[0] = [tensor.numpy() for tensor in result[0]]
-
-            # Convert tensors in result[1] to NumPy arrays
-            result[1] = [tensor.numpy() for tensor in result[1]]
-
-            # Convert the entire result list to NumPy array
-            result = np.array(result)
-            st.info(type(result))
-            f = result.shape
-            st.info(f)
-            result = result.reshape(3,1)
-            f = result.shape
-            st.info(f)
             st.image(result)
-
-
-
-
-            st.info('**TESTING**')
-            st.info(type(result))
-            c = result[0].shape
-            st.info(c)
-            r = result[1].shape
-            st.info(r)
-
-            numpy_array = result[1].numpy()
-            numpy_array = numpy_array[0].transpose(1, 2, 0)
-            
-            s = numpy_array.shape
-            st.info(s)
-            st.image(numpy_array)
-
-            # st.info(result)
-            st.info(len(result))
-            st.info(len(result[0]))
-            st.info(result[-1][-1][-1][-1][-1])
 
             
 
