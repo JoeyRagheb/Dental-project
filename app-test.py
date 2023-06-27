32ac2c636888f1724b7e58473a001d6afaff0c86
JoeyRagheb <101679327+JoeyRagheb@users.noreply.github.com>
2023-06-27 18:27:06 -0400

    t

--- app-test.py
+++ app-test.py
@@ -29,31 +29,7 @@ def processed_img(img_path):
     # source_folder = os.path.dirname(img_path) + '/'
     source_folder = img_path
     os.system(f'yolo task=segment mode=predict model=weights/best.pt conf=0.25 source={source_folder} save=true')
-    
-    # # Define the YOLO command
-    # yolo_command = "yolo task=segment mode=predict model=weights/best.pt conf=0.25 source=test-image/ save=true"
-
-    # # Execute the YOLO command and capture the output
-    # output = subprocess.check_output(yolo_command, shell=True)
-    # st.info('***HERE---------------------------***')
-    # st.info(output)
-
-    # # Process the output as needed
-    # # Here you can save the output to a variable or perform any further actions
-
-    # # # For example, if the output is an image file path
-    # image_path = output.decode().strip()  # Convert the output bytes to a string and remove leading/trailing whitespace
-    # st.info(image_path)
-
-    # # # Now you can use the image path or further process the image as needed
-    # # print(image_path)
-    
-
-
-
-
-
-    
+   
     folders = os.listdir('runs/segment/')
     folders = [re.findall(r'\d+', s) for s in folders]
     folders = [int(num) for sublist in folders for num in sublist]
